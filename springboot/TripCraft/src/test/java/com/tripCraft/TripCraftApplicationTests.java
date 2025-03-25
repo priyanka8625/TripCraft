77645f54c3d9f46167e542db7129b0755cbb31ae
AnkitaAdvitot <110318342+AnkitaAdvitot@users.noreply.github.com>
2025-03-25 14:48:31 +0530

    added backend

---
+++ springboot/TripCraft/src/test/java/com/tripCraft/TripCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
