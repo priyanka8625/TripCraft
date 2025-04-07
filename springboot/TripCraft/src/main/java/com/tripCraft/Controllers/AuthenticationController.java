c0a7c9109172a31626c300e047dd2adc70a6ec70
Priyanka Sundalam <ksundalam@gmail.com>
2025-04-07 23:14:46 +0530

    Updated status api

--- springboot/TripCraft/src/main/java/com/tripCraft/Controllers/AuthenticationController.java
+++ springboot/TripCraft/src/main/java/com/tripCraft/Controllers/AuthenticationController.java
@@ -60,6 +60,7 @@ public class AuthenticationController {
 	                    try {
 	                        String username = jwtService.extractUserName(token);
 	                        UserDetails user = userDetailsService.loadUserByUsername(username);
+
 	                        if (jwtService.validateToken(token, user)) {
 	                        	return ResponseEntity.ok().body(Map.of("authenticated", true));
 	                        }
@@ -68,9 +69,7 @@ public class AuthenticationController {
 	                    }
 	                }
 	            }
-	        }else {
-            	System.out.println("Np cookies");
-            }
+	        }
 
 	        return ResponseEntity.status(401).body("not authenticated");
 	    }
