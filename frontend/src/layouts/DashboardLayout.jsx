bcaa31474b3447a124b3d14d9b81c80f8eff6592
Priyanka Sundalam <ksundalam@gmail.com>
2025-04-11 14:20:50 +0530

    Updated Create-Trip form

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -16,7 +16,7 @@ const DashboardLayout = () => {
       <Sidebar />
       <div className="flex-1 overflow-auto">
         <Navbar user={user} />
-        <div className="p-4">
+        <div className="p-4 ">
           <Outlet />
         </div>
       </div>
--- frontend/src/routes/DashboardRoutes.jsx
+++ frontend/src/routes/DashboardRoutes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard/Dashboard';
-import PlanItinerary from '../pages/Dashboard/PlanItinerary';
 import DashboardLayout from '../layouts/DashboardLayout';
 import DisplayItinerary from '../pages/Dashboard/DisplayItinerary';
 import CreateTripForm from '../components/Dashboard/PlanItinerary/CreateTripForm';
