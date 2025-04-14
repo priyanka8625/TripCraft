565b4b50420ca318ccc1f7b7b7c1e2b67ab38eee
Priyanka Sundalam <ksundalam@gmail.com>
2025-04-14 13:03:02 +0530

    Created a selection form for planning itinerary

--- frontend/src/routes/DashboardRoutes.jsx
+++ frontend/src/routes/DashboardRoutes.jsx
@@ -3,14 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import DashboardLayout from '../layouts/DashboardLayout';
 import DisplayItinerary from '../pages/Dashboard/DisplayItinerary';
-import CreateTripForm from '../components/Dashboard/PlanItinerary/CreateTripForm';
+import CreateTripForm from '../pages/Dashboard/PlanItinerary/CreateTripForm';
+import PlanMethodSelection from '../pages/Dashboard/PlanItinerary/PlanMethodSelection';
 
 function DashboardRoutes() {
   return (
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Dashboard />} />
-        <Route path="create-trip" element={<CreateTripForm />} />
+        <Route path="plan" element={<PlanMethodSelection />} />
+        <Route path="plan/ai" element={<CreateTripForm />} />
+        <Route path="plan/manual" element={<CreateTripForm />} />
         <Route path="itinerary" element={<DisplayItinerary />} />
       </Route>
     </Routes>
