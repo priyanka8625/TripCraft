cfe31284c056a3cb488e1f4dded1a4f289fe9ed3
Rocking-ui <ingrock737@gmail.com>
2025-04-25 22:17:59 +0530

    Update Logo.jsx added palm tree

--- frontend/src/components/Dashboard/Sidebar/Logo.jsx
+++ frontend/src/components/Dashboard/Sidebar/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="p-6 flex items-center gap-2">
       <div className="bg-emerald-600 p-2 rounded-lg">
-        <Compass className="h-6 w-6 text-white" />
+       <img src="./../public/img/tree-palm-white.png" alt="" className='w-6 h-6'/>
       </div>
       <span className="text-2xl font-bold bg-gradient-to-r from-emerald-600 to-emerald-400 bg-clip-text text-transparent">
         TripCraft
